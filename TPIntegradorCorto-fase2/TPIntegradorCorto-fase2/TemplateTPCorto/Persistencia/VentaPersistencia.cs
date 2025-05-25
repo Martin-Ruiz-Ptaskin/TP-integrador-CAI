abb35a4a496692d6493be4e61284bc449742788e
Juanpi784 <juapicuneo12@gmail.com>
2025-05-25 11:17:50 -0300

    Add files via upload

---
+++ TPIntegradorCorto-fase2/TPIntegradorCorto-fase2/TemplateTPCorto/Persistencia/VentaPersistencia.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Persistencia.WebService.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public class VentaPersistencia
+    {
+        private Guid idUsuario = new Guid("0cdbc5a5-69d9-4ab8-8cb3-9932ce33f54a");
+
+        /*
+        public bool agregarVenta(venta)
+        {
+            var jsonRequest = JsonConvert.SerializeObject(venta);
+
+            HttpResponseMessage response = WebHelper.Post("Venta/AgregarVenta", jsonRequest);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        */
+    }
+}
